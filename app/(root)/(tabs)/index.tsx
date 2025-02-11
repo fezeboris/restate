e31bdf52c13492cf9aadb18df9ae09011a184a66
fezeboris <borisfeze9@gmail.com>
2025-02-11 15:06:40 +0100

    Tablayout set up

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-lg text-blue-400 font-bold font-rubik-extrabold">Welcome Restate</Text>
+
+    </View>
+  );
+}
